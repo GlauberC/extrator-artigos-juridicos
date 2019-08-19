31723446c70594a93bb27a14970b2ec154695a50
glauberc <glaubercarv@gmail.com>
2019-08-19 01:53:37 -0300

    new design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,108 @@
-import React, { useState } from 'react';
-import api from './services/api'
-import './app.css'
+import React, { useState } from "react";
+import api from "./services/api";
+import "./styles/reset.css";
+import "./styles/style.scss";
 
 // palavras_selecionadas = ["prisao", "crime", "contrato", "indenizacao", "tutela", "saude", "consumidor", "trafico", "cobranca", "reforma", "drogas", "habeas", "homicidio", "cautelar", "servidor", "constrangimento", "servico", "seguranca", "posse", "concurso", "razoabilidade", "roubo", "violacao", "vicios", "contradicao", "provimento", "militar", "probatorio", "financeira", "flagrante", "apreensao", "cargo", "administracao", "mandado", "prestacao", "cartao", "autoral", "social", "policial", "imovel", "banco", "remuneracao", "prejuizo", "carreira", "veiculo", "taxa", "legalidade", "ofensa", "natureza", "associacao", "empresa", "absolvicao", "salario", "remessa", "devedor", "participacao", "aumento", "custodia", "divida", "anulacao"]
 
 export default App => {
-  const [palavras, setPalavras] = useState('')
-  const [artigos, setArtigos] = useState([])
+  const [palavras, setPalavras] = useState("");
+  const [artigos, setArtigos] = useState([]);
 
-  const handleClick = async (e) => {
-    e.preventDefault()
+  const handleClick = async e => {
+    console.log(artigos);
+    e.preventDefault();
     const data = {
-      "palavras": palavras.replace(/\s+/ig, ' ').split(' ')
-    }
-    const response = await api.post('/artigos',
-      data
-    )
-    let listaArtigos = []
+      palavras: palavras.replace(/\s+/gi, " ").split(" ")
+    };
+    const response = await api.post("/artigos", data);
+    let listaArtigos = [];
     for (let keys in response.data) {
-      listaArtigos.push({ nome: keys, ocorrencia: response.data[keys] })
+      listaArtigos.push({ nome: keys, ocorrencia: response.data[keys] });
     }
-    listaArtigos.sort(function (a, b) {
+    listaArtigos.sort(function(a, b) {
       return b.ocorrencia - a.ocorrencia;
-    })
-    setArtigos(listaArtigos)
-  }
+    });
+    setArtigos(listaArtigos);
+  };
 
   return (
-    <div className="App">
-      <div className="container">
-
-        <form>
-          <input className="palavras" value={palavras}
-            onChange={(e) => setPalavras(e.target.value.toLowerCase().replace(/[,.]/ig, ''))}
-            placeholder="Digite as palavras aqui" />
-          <button className="btn-enviar" type="submit" onClick={e => handleClick(e)}>enviar</button>
-          <div className="chips"></div>
-        </form>
-        <h2>Sugestões de artigos:</h2>
-        <ul>
-          {artigos.map(artigo => (
-            <li key={artigo.nome}>{artigo.nome}</li>
-          ))}
-        </ul>
-
-      </div>
+    <div className="interface">
+      <header className="header">
+        <div className="container">
+          <h1>
+            Extrator de Artigos
+            <br /> Jurídicos
+          </h1>
+          <nav>
+            <a href="#">sobre</a>
+          </nav>
+        </div>
+      </header>
+      <main>
+        <section className="introducao">
+          <p>
+            A forma mais fácil de encontrar
+            <br /> artigos jurídicos
+          </p>
+        </section>
+        <section className="palavras">
+          <h2>Digite as palavras chaves</h2>
+          <textarea
+            value={palavras}
+            onChange={e =>
+              setPalavras(
+                e.target.value
+                  .toLowerCase()
+                  .replace(/[,.]/gi, "")
+                  .replace(/ç/gi, "c")
+              )
+            }
+            placeholder="ex: banco concurso salario"
+            cols="80"
+            rows="5"
+          />
+          <button type="submit" onClick={e => handleClick(e)}>
+            consultar
+          </button>
+        </section>
+        <section className="artigos">
+          <div className="container">
+            <ul>
+              {artigos.length !== 0 ? (
+                artigos.map(artigo => <li key={artigo.nome}>{artigo.nome}</li>)
+              ) : (
+                <li>Nenhuma consulta feita</li>
+              )}
+            </ul>
+          </div>
+        </section>
+      </main>
+      <footer className="rodape">
+        <div className="container">
+          <p>GC APPs / UFRN - Alguns direitos reservados</p>
+        </div>
+      </footer>
     </div>
   );
+};
+
+{
+  /* <div className="container">
+
+<form>
+  <input className="palavras" value={palavras}
+    onChange={(e) => setPalavras(e.target.value.toLowerCase().replace(/[,.]/ig, ''))}
+    placeholder="Digite as palavras aqui" />
+  <button className="btn-enviar" type="submit" onClick={e => handleClick(e)}>enviar</button>
+  <div className="chips"></div>
+</form>
+<h2>Sugestões de artigos:</h2>
+<ul>
+  {artigos.map(artigo => (
+    <li key={artigo.nome}>{artigo.nome}</li>
+  ))}
+</ul>
+
+</div> */
 }
